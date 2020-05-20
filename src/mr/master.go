f88191a890b8e1a534f18775081e3a8baf8b6b2d
Sebastian Schwartz <sebastian.a.schwartz@gmail.com>
2020-05-20 17:20:09 +1000

    updating logging

--- src/mr/master.go
+++ src/mr/master.go
@@ -90,7 +90,7 @@ func (m *Master) GetTask(args *GetTaskArgs, reply *GetTaskReply) error {
 				time.Now().Sub(task.executionStart) > TaskTimeoutTime) {
 
 			if time.Now().Sub(task.executionStart) > TaskTimeoutTime {
-				fmt.Printf("Master: timeout on task, reassigning: id %v, type %v", reply.Id, args.Type)
+				fmt.Printf("Master: timeout on task, reassigning: id %v, type: %v\n", reply.Id, args.Type)
 			}
 
 			m.tasks[i].status = Running
@@ -98,13 +98,13 @@ func (m *Master) GetTask(args *GetTaskArgs, reply *GetTaskReply) error {
 			reply.Id = m.tasks[i].id
 			reply.Files = m.tasks[i].files
 			reply.NReduceTasks = m.nReduceTasks
-			reply.Msg = fmt.Sprintf("RPC GetTask: task: id %v, type %v", reply.Id, args.Type)
+			reply.Msg = fmt.Sprintf("RPC GetTask: task: id %v, type: %v", reply.Id, args.Type)
 			return nil
 		}
 	}
 
 	// no task available
-	reply.Msg = fmt.Sprintf("RPC GetTask: no task of type %v available.", args.Type)
+	reply.Msg = fmt.Sprintf("RPC GetTask: no task of type: %v available.", args.Type)
 	return nil
 }
 
