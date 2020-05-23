d205217d5e44427fa55871a9d389b8d72ee5b131
Sebastian Schwartz <sebastian.a.schwartz@gmail.com>
2020-05-23 16:19:28 +1000

    task timeout

--- src/mr/master.go
+++ src/mr/master.go
@@ -22,7 +22,7 @@ const (
 
 // how long before assuming a crash has occurred
 const (
-	TaskTimeoutTime time.Duration = (time.Second * 5)
+	TaskTimeoutTime time.Duration = (time.Second * 10)
 )
 
 type TaskType int
